c12c7aeef1d78e18e506ce505f5aea7e8a45203f
tyleryonke <tyleryonke@gmail.com>
2017-03-17 09:44:42 -0500

    jawsdb changes

--- app/config/db.js
+++ app/config/db.js
@@ -1,22 +1,16 @@
-{
-  "development": {
-    "username": "root",
-    "password": "Timothy92",
-    "database": "burger_sequel",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "donteatalone",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "cbbhs5pscvz78ga6",
-    "password": "jp8ndbjerru1eliq",
-    "database": "twxlk9tq3uux1a22",
-    "host": "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    "dialect": "mysql"
+// Dependencies
+var Sequelize = require("sequelize");
+
+// Creates mySQL connection using Sequelize
+var db = new Sequelize("donteatalone", "cbbhs5pscvz78ga6", "jp8ndbjerru1eliq", {
+  host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
+});
+
+// Exports the connection for other files to use
+module.exports = db;
